1b3a2f0104d2edbb357b516182df0844d00f4749
Steve Schneider <steve@sunyit.edu>
2017-08-13 15:46:03 -0400

    got filter[toc] working in publication 3

---
+++ amgov-info-age/scripts/build-index.sh
@@ -0,0 +1,3 @@
+tiddlywiki --load ../../original-wiki/index.html
+tiddlywiki --load ../
+tiddlywiki ../ --output ../. --build index
---
+++ amgov-info-age/sunypoly-epup/build-index.sh
@@ -0,0 +1,3 @@
+tiddlywiki --verbose --load ../../original-wiki/index.html --load amgov-tiddlers.json --rendertiddler $:/core/save/all amgov-wiki.html text/plain
+
+
